9259fa1d8a8f77cf49a5fbcffd0eff8b07f84ec6
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-31 11:24:59 +0200

    - Fixed bugs - Added my logo - Added my skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/pages/home-page/hero/Hero";
+import Skills from "@/components/pages/home-page/skills/Skills";
 
 export default function Home() {
     return (
         <main className="h-full w-full">
             <div className="flex flex-col gap-20">
                 <Hero/>
+                <Skills/>
             </div>
         </main>
     );
