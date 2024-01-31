60b9f0243924933e83b90257dd1e4a6c3e193cf8
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-31 12:52:48 +0200

    - skills components

--- src/components/pages/home-page/skills/Skills.tsx
+++ src/components/pages/home-page/skills/Skills.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Frontend_skill} from "../../../../../constans";
 import SkillDataProvider from "@/components/pages/home-page/skills/skill-data-provider/SkillDataProvider";
+import SkillText from "@/components/pages/home-page/skills/skill-text/SkillText";
 
 const Skills = () => {
     return (
@@ -9,6 +10,7 @@ const Skills = () => {
             className={'flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20'}
             style={{transform: "scale(.9)"}}
         >
+            <SkillText/>
             <div className={'flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'}>
                 {Frontend_skill.map((image, index) => (
                     <SkillDataProvider
@@ -19,6 +21,23 @@ const Skills = () => {
                         index={index}
                         alt={image.skill_name}/>
                 ))}
+            </div>
+
+            <div className={'w-full h-full absolute'}>
+                <div className={'w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover'}>
+                    <video
+                        className={'w-full h-auto'}
+                        preload={'false'}
+                        playsInline
+                        loop
+                        muted
+                        autoPlay
+                        src={'/cards-video.webm'}
+
+                    >
+
+                    </video>
+                </div>
             </div>
 
         </section>
