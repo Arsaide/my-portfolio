039c5f498fbc90c2df795bc7014335a1566dd40d
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-02-04 17:21:02 +0200

    - edited navbar

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import Image from "next/image";
 import {Socials} from "../../../../constans";
+import {logoImg} from "@/media/images";
+import Link from "next/link";
 
 const Navbar = () => {
     return (
         <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
             <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-                <a href="#about-me"
-                    className="h-auto w-auto flex flex-row items-center">
+                <Link href="https://github.com/Arsaide"
+                    className="cursor-pointer h-auto w-[120px] flex flex-row items-center hover-opacity transition-opacity duration-300 ease-in-out">
                     <Image
-                        src={'/logo.svg'}
+                        src={logoImg}
                         alt={'Logo'}
-                        width={70}
-                        height={70}
-                        className={'cursor-pointer hover:animate-slowspin'}
+                        width={50}
+                        height={50}
+                        loading={'lazy'}
                     />
 
                     <span className={'font-bold ml-[10px] hidden md:block text-gray-300'}>
                         Arsaide
                     </span>
-                </a>
+                </Link>
 
-                <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-                    <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-                        <a href="#about-me" className="cursor-pointer">
-                            About me
+                <div className="w-auto h-full flex flex-row items-center justify-between">
+                    <div className="flex items-center justify-between gap-[80px] w-full h-auto border border-[#7042f861] bg-[#0300145e] px-[20px] py-[10px] rounded-full text-gray-200">
+                        <a href="#about" className="cursor-pointer">
+                            About
                         </a>
                         <a href="#skills" className="cursor-pointer">
                             Skills
@@ -35,14 +37,19 @@ const Navbar = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-row gap-5">
+                <div className="flex flex-row gap-5 w-[120px]">
                     {Socials.map((item) => (
-                        <Image
-                            src={item.src}
-                            alt={item.name}
-                            key={item.name}
-                            width={24}
-                            height={24}/>
+                        <Link
+                            href={item.link}>
+                            <Image
+                                src={item.src}
+                                alt={item.name}
+                                key={item.name}
+                                width={24}
+                                height={24}
+                                loading={'lazy'}
+                            />
+                        </Link>
                     ))}
                 </div>
             </div>
