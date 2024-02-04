6445c4aca0df0e2f579f64019e6d0865dd915f58
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-02-04 17:21:41 +0200

    - edited project section

--- src/components/pages/home-page/projects/Projects.tsx
+++ src/components/pages/home-page/projects/Projects.tsx
@@ -4,12 +4,14 @@ import ProjectCnt from "@/components/pages/home-page/projects/components/project
 
 const Projects = () => {
     return (
-        <div className={'flex flex-col items-center justify-center py-20'}>
+        <section
+            className={'flex flex-col items-center justify-center py-20'}
+            id={'projects'}>
             <h3 className={'text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20'}>
                 My Projects
             </h3>
             <ProjectCnt/>
-        </div>
+        </section>
     );
 };
 
